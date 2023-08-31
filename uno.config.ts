a5d60e17f1655afbf31a27c691fc975b9d8534e3
Andrew Kachnic <ajkachnic@protonmail.com>
2023-08-31 16:13:41 -0400

    fix font loading

--- uno.config.ts
+++ uno.config.ts
@@ -1,17 +1,15 @@
 // uno.config.ts
-import { defineConfig, transformerDirectives } from 'unocss'
+import { defineConfig, transformerDirectives } from "unocss";
 
 export default defineConfig({
-  transformers: [
-    transformerDirectives()
-  ],
+  transformers: [transformerDirectives()],
   rules: [
     // your custom rules
   ],
   theme: {
     fontFamily: {
-      serif: "Instrument Serif",
-      sans: "Switzer"
-    }
-  }
-})
+      serif: "Instrument Serif, serif",
+      sans: "Switzer",
+    },
+  },
+});
