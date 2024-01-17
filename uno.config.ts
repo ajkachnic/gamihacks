099debbdba6a2f0f767d6ac2768139512e142c83
Andrew Kachnic <ajkachnic@protonmail.com>
2024-01-16 21:27:30 -0500

    font fallback

--- uno.config.ts
+++ uno.config.ts
@@ -2,16 +2,16 @@
 import { defineConfig, transformerDirectives } from "unocss";
 
 export default defineConfig({
-	transformers: [transformerDirectives()],
-	rules: [
-		// your custom rules
-	],
-	theme: {
-		fontFamily: {
-			serif: "Instrument Serif, serif",
-			// sans: "Switzer",
-			sans: "Neue Montreal",
-			// sans: "Inter",
-		},
-	},
+  transformers: [transformerDirectives()],
+  rules: [
+    // your custom rules
+  ],
+  theme: {
+    fontFamily: {
+      serif: "Instrument Serif, serif",
+      // sans: "Switzer",
+      sans: "Neue Montreal, Inter, sans-serif",
+      // sans: "Inter",
+    },
+  },
 });
