89ff8fc504b9a92e3b5a4f93c6c16e6b4c64ec3d
Andrew Kachnic <ajkachnic@protonmail.com>
2024-01-16 21:26:05 -0500

    update website (finally)

--- uno.config.ts
+++ uno.config.ts
@@ -2,14 +2,16 @@
 import { defineConfig, transformerDirectives } from "unocss";
 
 export default defineConfig({
-  transformers: [transformerDirectives()],
-  rules: [
-    // your custom rules
-  ],
-  theme: {
-    fontFamily: {
-      serif: "Instrument Serif, serif",
-      sans: "Switzer",
-    },
-  },
+	transformers: [transformerDirectives()],
+	rules: [
+		// your custom rules
+	],
+	theme: {
+		fontFamily: {
+			serif: "Instrument Serif, serif",
+			// sans: "Switzer",
+			sans: "Neue Montreal",
+			// sans: "Inter",
+		},
+	},
 });
